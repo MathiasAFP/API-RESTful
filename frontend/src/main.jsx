e1ec299bc148944b1fb795738dd0d12f620034b2
Matheus Polinski <matheus.filippipolinski@gmail.com>
2025-11-04 11:18:27 -0300

    tailwind css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Dashboard from './components/Dashboard'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
