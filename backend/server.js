79ef9505bf07842a702ca610e4868802bf6ee7a9
Felipe Bordignon <felipebordignon44@gmail.com>
2025-11-13 08:35:34 -0300

    teste(backend)  conexão banco de dados

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+
+dotenv.config();
+
+const app = express();
+const PORT = 3000;
+
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGOKEY);
+        console.log("Conectado No Banco");
+    } catch (error) {
+        console.log("Deu erro na conexão do banco", error)
+    }
+};
+
+connectDB();
