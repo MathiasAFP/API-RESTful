cf5c0cadcad791ec526065a0b3036eb4bd67b5d3
Luan Martini <luancarlosmartini@gmail.com>
2025-11-22 12:11:33 -0300

    Revert "..."
    This reverts commit dbbc7ff0d3b00baa2a447e1dd5e1a7d51d28d958.

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-const projetosRoutes = require("../backend/routes/projetos.routes");
-
 app.use(express.json());
-app.use("/projetos", projetosRoutes);
 
 mongoose.connect(process.env.MONGOKEY)
     .then(() => console.log("MongoDB conectado"))
     .catch(err => console.log("Erro ao conectar Mongo:", err));
+
+app.use("/projetos", require("./routes/projetos.routes"));
 
 app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
